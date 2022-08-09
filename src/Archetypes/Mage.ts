080d6a1faf3cc6b781cb679bad1b81c84d25c762
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-09 17:30:58 -0300

    feat: create classes that inherit from archetype

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  private _energyType: EnergyType;
+  private static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+  }
+
+  override get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this.instances += 1;
+    return this.instances;
+  }
+}
+
+export default Mage;
---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+  }
+
+  override get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this.instances += 1;
+    return this.instances;
+  }
+}
+
+export default Warrior;
