5c1484fee242031f3b0f0a18d9b77dbc5e61a9f3
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-09 16:43:24 -0300

    feat: create class that inherit from race

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Halfling extends Race {
+  private _maxLifePoints: number;
+  private static _createdInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+  }
+
+  override get maxLifePoints() {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    this._createdInstances += 1;
+    return this._createdInstances;
+  }
+}
+
+export default Halfling;
